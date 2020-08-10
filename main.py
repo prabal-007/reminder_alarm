c33f1e7ad5a1036917be331341dbe76ce6bb0953
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-08-10 23:43:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
- 
- 
+import time
+import winsound
+from win10toast import ToastNotifier
+
+def timer(reminder, minutes):
+    notificator = ToastNotifier()
+
+    notificator.show_toast('Reminder',
+    f"""Alarm will turn off in {minutes} mins.""", duration=50)
+
+    time.sleep(minutes * 60)
+
+    notificator.show_toast(f'Reminder', reminder, duration=50)
+
+    #Alarm
+    frequency = 2500  #in Hz
+    duration = 1000 # 1 sec
+    winsound.Beep(frequency, duration)
+
+if __name__ == "__main__":
+    words = input('what would you remindes of: ')
+    mins = int(input('Enter minutes: '))
+    timer(words, mins)
